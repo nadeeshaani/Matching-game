73e6799c389e549e70cb4cae83172f10ae6e147e
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-04 20:02:30 +0530

    matchgame

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,36 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🦖", "🦖",
+                "🐳","🐳",
+                "🐴","🐴",
+                "🐧","🐧",
+                "🦣","🦣",
+                "🦀","🦀",
+                "🦉","🦉",
+                "🐛","🐛",
+            };
+
+            Random random = new Random();
+
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
+
+
+
         }
     }
 }
